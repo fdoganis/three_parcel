3dd0bbf5d3479965e7a845985db9d4cee2005474
fdoganis <fivos.doganis@gmail.com>
2023-02-09 19:03:40 +0100

    Ambient light

--- src/main.js
+++ src/main.js
@@ -1,80 +1,83 @@
-   import * as THREE from 'three'
+import * as THREE from 'three'
 
-   // NOTE: three/addons alias or importmap does not seem to be supported by Parcel, use three/examples/jsm/ instead 
+// NOTE: three/addons alias or importmap does not seem to be supported by Parcel, use three/examples/jsm/ instead 
 
-   import {
-     OrbitControls
-   } from 'three/examples/jsm/controls/OrbitControls.js';
+import {
+  OrbitControls
+} from 'three/examples/jsm/controls/OrbitControls.js';
 
-   import {
-     GLTFLoader
-   } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import {
+  GLTFLoader
+} from 'three/examples/jsm/loaders/GLTFLoader.js';
 
-   // Example of hard link to official repo for data, if needed
-   // const MODEL_PATH = 'https://raw.githubusercontent.com/mrdoob/three.js/r148/examples/models/gltf/LeePerrySmith/LeePerrySmith.glb';
+// Example of hard link to official repo for data, if needed
+// const MODEL_PATH = 'https://raw.githubusercontent.com/mrdoob/three.js/r148/examples/models/gltf/LeePerrySmith/LeePerrySmith.glb';
 
 
-   // INSERT CODE HERE
+// INSERT CODE HERE
 
-   const scene = new THREE.Scene();
-   const aspect = window.innerWidth / window.innerHeight;
-   const camera = new THREE.PerspectiveCamera(75, aspect, 0.1, 1000);
+const scene = new THREE.Scene();
+const aspect = window.innerWidth / window.innerHeight;
+const camera = new THREE.PerspectiveCamera(75, aspect, 0.1, 1000);
 
-   const renderer = new THREE.WebGLRenderer();
-   renderer.setSize(window.innerWidth, window.innerHeight);
-   document.body.appendChild(renderer.domElement);
+const light = new THREE.AmbientLight(0x404040); // soft white light
+scene.add(light);
 
-   const controls = new OrbitControls(camera, renderer.domElement);
-   controls.listenToKeyEvents(window); // optional
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
-   const geometry = new THREE.BoxGeometry(1, 1, 1);
-   const material = new THREE.MeshNormalMaterial();
-   const cube = new THREE.Mesh(geometry, material);
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.listenToKeyEvents(window); // optional
 
-   scene.add(cube);
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshNormalMaterial();
+const cube = new THREE.Mesh(geometry, material);
+
+scene.add(cube);
 
 function loadData() {
-    new GLTFLoader()
-        .setPath('assets/models/')
-        .load('test.glb', gltfReader);
+ new GLTFLoader()
+     .setPath('assets/models/')
+     .load('test.glb', gltfReader);
 }
 
 
 function gltfReader(gltf) {
-    let testModel = null;
+ let testModel = null;
 
-    testModel = gltf.scene;
+ testModel = gltf.scene;
 
-    if (testModel != null) {
-        console.log("Model loaded:  " + testModel);
-       scene.add(gltf.scene);
-    } else {
-        console.log("Load FAILED.  ");
-    }
+ if (testModel != null) {
+     console.log("Model loaded:  " + testModel);
+    scene.add(gltf.scene);
+ } else {
+     console.log("Load FAILED.  ");
+ }
 }
 
 loadData();
 
 
-   camera.position.z = 3;
+camera.position.z = 3;
 
 
-   const clock = new THREE.Clock();
+const clock = new THREE.Clock();
 
-    // Main loop
-   const animation = () => {
+ // Main loop
+const animation = () => {
 
-     renderer.setAnimationLoop(animation); // requestAnimationFrame() replacement, compatible with XR 
+  renderer.setAnimationLoop(animation); // requestAnimationFrame() replacement, compatible with XR 
 
-     const delta = clock.getDelta();
-     const elapsed = clock.getElapsedTime();
+  const delta = clock.getDelta();
+  const elapsed = clock.getElapsedTime();
 
-     // can be used in shaders: uniforms.u_time.value = elapsed;
+  // can be used in shaders: uniforms.u_time.value = elapsed;
 
-     cube.rotation.x = elapsed / 2;
-     cube.rotation.y = elapsed / 1;
-     
-     renderer.render(scene, camera);
-   };
+  cube.rotation.x = elapsed / 2;
+  cube.rotation.y = elapsed / 1;
 
-   animation();
+  renderer.render(scene, camera);
+};
+
+animation();

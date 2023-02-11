451fa55e3c174cadc3dd23256b03736444eee776
fdoganis <fivos.doganis@gmail.com>
2023-02-11 09:30:55 +0100

    birghter ambient light

--- src/main.js
+++ src/main.js
@@ -20,7 +20,7 @@ const scene = new THREE.Scene();
 const aspect = window.innerWidth / window.innerHeight;
 const camera = new THREE.PerspectiveCamera(75, aspect, 0.1, 1000);
 
-const light = new THREE.AmbientLight(0x404040); // soft white light
+const light = new THREE.AmbientLight(0xffffff, 1.0); // soft white light
 scene.add(light);
 
 const renderer = new THREE.WebGLRenderer();
